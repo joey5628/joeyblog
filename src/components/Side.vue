68bb00f9e228702f6493ac86e020b1788988447f
zhangyi <zhangyi5628@126.com>
2018-01-07 20:44:12 +0800

    side

--- src/components/Side.vue
+++ src/components/Side.vue
@@ -1,25 +1,24 @@
 <template>
-    <el-aside class="common-side-wrapper">
-            <div class="retract">
-                <i class="el-icon-arrow-left"></i>
-            </div>
-            <div class="common-side">
-                <div class="name-card">
-                    <div class="photo-wrapper">
-                        <img src="../assets/img/sun.jpg" alt="">
-                    </div>
+    <el-aside class="common-side-wrapper" :class="{retract: isRetract}">
+        <div class="btn-retract" v-on:click="retractToggle">
+            <i class="el-icon-arrow-left"></i>
+        </div>
+        <div class="common-side">
+            <div class="name-card">
+                <div class="photo-wrapper">
+                    <img src="../assets/img/sun.jpg" alt="">
                 </div>
-                <ul class="link-wrapper">
-                    <li v-for="(link, index) in links"
-                        :key="index"
-                        class="link">
-                        <router-link :to="link.path">
-                            <i class="iconfont" :class="link.icon"></i>
-                            <span>{{link.name}}</span>
-                        </router-link>
-                    </li>
-                </ul>
             </div>
+            <ul class="link-wrapper">
+                <li v-for="(link, index) in links"
+                    :key="index">
+                    <router-link :to="link.path" class="link">
+                        <i class="iconfont" :class="link.icon"></i>
+                        <span>{{link.name}}</span>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
     </el-aside>
 </template>
 
@@ -27,37 +26,37 @@
     export default {
         data () {
             return {
+                isRetract: false,
                 links: [{
                     name: '首页',
                     path: '/home',
                     icon: 'icon-icon-f1',
-                    isActive: true
                 }, {
                     name: '文章',
                     path: '/article',
                     icon: 'icon-article',
-                    isActive: false
                 }, {
                     name: '分类',
                     path: '/category',
                     icon: 'icon-icon03',
-                    isActive: false
                 }, {
                     name: '标签',
                     path: '/tag',
                     icon: 'icon-tag',
-                    isActive: false
                 }, {
                     name: '评论',
                     path: '/comment',
                     icon: 'icon-comment',
-                    isActive: false
                 }]
             }
         },
 
         methods: {
-
+            retractToggle () {
+                console.log(111)
+                this.isRetract = !this.isRetract
+                console.log('this.isRetract:', this.isRetract)
+            }
         }
     }
 </script>
@@ -65,15 +64,19 @@
 <style lang="less">
     @import "../assets/css/default.less";
 
+    .retractTransition () {
+        transition: all 0.3s;
+    }
+
     .common-side-wrapper {
         width: 200px !important;
         background-color: @side-fill;
-        overflow: hidden;
         box-shadow: 0 0 5px @side-fill;
-        position: relative;
         z-index: 2;
+        .retractTransition;
 
-        .retract {
+        .btn-retract {
+            cursor: pointer;
             position: absolute;
             z-index: 1;
             top: 50%;
@@ -94,61 +97,109 @@
                 font-weight: bold;
                 color: #fff;
                 margin-left: 22px;
+                .retractTransition;
             }
         }
         &:hover {
-            .retract {
+            .btn-retract {
                 left: 172px;
             }
         }
 
-    }
-    .common-side {
-        height: 100%;
-        position: relative;
-        background-color: @side-fill;
-        z-index: 2;
+        .common-side {
+            height: 100%;
+            position: relative;
+            background-color: @side-fill;
+            z-index: 2;
 
-        .name-card {
-            .photo-wrapper {
-                margin: auto;
-                padding: 20px;
-                width: 100px;
-                height: 100px;
+            .name-card {
+                padding: 20px 0;
 
-                img {
-                    width: 100%;
-                    height: 100%;
-                    border-radius: 50%;
+                .photo-wrapper {
+                    margin: auto;
+                    /*padding: 20px;*/
+                    width: 100px;
+                    height: 100px;
+                    .retractTransition;
+
+                    img {
+                        width: 100%;
+                        height: 100%;
+                        border-radius: 50%;
+                    }
                 }
             }
 
+            a.link {
+                height: 50px;
+                line-height: 50px;
+                color: #fff;
+                font-size: 16px;
+                display: block;
+                padding: 0 50px;
+                opacity: .75;
+                overflow: hidden;
 
+                &:hover {
+                    background-color: rgba(36, 168, 232, .3);
+                }
+                i.iconfont {
+                    margin-right: 15px;
+                    .retractTransition;
+                }
+
+                span {
+                    opacity: 1;
+                    height: 100%;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    .retractTransition;
+                }
+
+                &.router-link-active {
+                    background-color: @brand-primary;
+                    color: #fff;
+                    opacity: 1;
+                }
+            }
         }
 
-        .link-wrapper {
-            .link {
-                a {
-                    height: 50px;
-                    line-height: 50px;
-                    display: block;
-                    color: #fff;
-                    font-size: 16px;
-                    padding: 0 50px;
-                    opacity: .75;
+        &.retract {
+            width: 60px !important;
+
+            .btn-retract {
+                left: 0;
+                i {
+                    transform: rotate(180deg);
+                }
+
+            }
+
+            &:hover {
+                .btn-retract {
+                    left: 32px;
+                }
+            }
+
+            .common-side {
+                .name-card {
+                    .photo-wrapper {
+                        width: 40px;
+                        height: 40px;
+                    }
+                }
+
+                a.link {
+                    padding: 0 19px;
+                    span {
+                        opacity: 0;
+                    }
 
                     .iconfont {
-                        margin-right: 15px;
-                    }
-
-                    &.router-link-active {
-                        background-color: @brand-primary;
-                        color: #fff;
-                        opacity: 1;
+                        font-size: 20px;
                     }
                 }
             }
         }
-
     }
 </style>
